d960ca5c440053714c56693f92889b5847e4d792
Hasan Aydoğdu <hsnaydd@gmail.com>
2017-03-18 03:40:08 +0300

    set lowercase jsdoc params

--- src/moveTo.js
+++ src/moveTo.js
@@ -2,7 +2,7 @@
 const MoveTo = (() => {
   /**
    * Defaults
-   * @type {Object}
+   * @type {object}
    */
   const defaults = {
     tolerance: 0,
@@ -13,11 +13,11 @@ const MoveTo = (() => {
 
   /**
    * easeOutQuart Easing Function
-   * @param  {Integer} t - current time
-   * @param  {Integer} b - start value
-   * @param  {Integer} c - change in value
-   * @param  {Integer} d - duration
-   * @return {Integer} - calculated value
+   * @param  {number} t - current time
+   * @param  {number} b - start value
+   * @param  {number} c - change in value
+   * @param  {number} d - duration
+   * @return {number} - calculated value
    */
   function easeOutQuart(t, b, c, d) {
     t /= d;
@@ -27,8 +27,8 @@ const MoveTo = (() => {
 
   /**
    * Returns html element's top and left offset
-   * @param  {Node} elem - Element
-   * @return {Object} Element top and left offset
+   * @param  {HTMLElement} elem - Element
+   * @return {object} Element top and left offset
    */
   function getOffsetSum(elem) {
     let top = 0;
@@ -46,9 +46,9 @@ const MoveTo = (() => {
   /**
    * Merge two object
    *
-   * @param  {Object} obj1
-   * @param  {Object} obj2
-   * @return {Object} merged object
+   * @param  {object} obj1
+   * @param  {object} obj2
+   * @return {object} merged object
    */
   function mergeObject(obj1, obj2) {
     let obj3 = {};
@@ -69,14 +69,13 @@ const MoveTo = (() => {
   /**
    * Converts camel case to kebab case
    * @param  {string} val the value to be converted
-   * @return {String} the converted value
+   * @return {string} the converted value
    */
   function kebabCase(val) {
     return val.replace(/([A-Z])/g, function($1) {
       return '-' + $1.toLowerCase();
     });
   };
-
 
   /**
    * Scrolls to an element
@@ -96,7 +95,7 @@ const MoveTo = (() => {
     /**
      * Register a dom element as trigger
      * @param  {HTMLElement} dom Dom trigger element
-     * @param  {Function} callback Callback function
+     * @param  {function} callback Callback function
      */
     registerTrigger(dom, callback) {
       if (!dom) {
@@ -123,8 +122,8 @@ const MoveTo = (() => {
     /**
      * Move
      * Scrolls to given element by using easeOutQuart function
-     * @param  {HTMLElement|Number} target Target element to be scrolled or target position
-     * @param  {Object} options Custom options
+     * @param  {HTMLElement|number} target Target element to be scrolled or target position
+     * @param  {object} options Custom options
      */
     move(target, options = {}) {
       if (target !== 0 && !target) {
@@ -183,8 +182,8 @@ const MoveTo = (() => {
 
     /**
      * Adds custom ease function
-     * @param {String}   name Ease function name
-     * @param {Function} fn   Ease Function
+     * @param {string}   name Ease function name
+     * @param {function} fn   Ease function
      */
     addEaseFunction(name, fn) {
       this.easeFunctions[name] = fn;
@@ -194,8 +193,8 @@ const MoveTo = (() => {
   /**
    * Returns options which created from trigger dom element
    * @param  {HTMLElement} dom Trigger dom element
-   * @param  {Object} options The instance's options
-   * @return {Object} The options which created from trigger dom element
+   * @param  {object} options The instance's options
+   * @return {object} The options which created from trigger dom element
    */
   function _getOptionsFromTriggerDom(dom, options) {
     const domOptions = {};
